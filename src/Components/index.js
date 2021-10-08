669f99978ce6d90b6106c4966ef515118fe6b8ee
Yuri Simonov <y.simonov@alef.im>
2021-10-08 16:55:38 +0300

    progress

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,5 @@
-export {default as Home}  from "./Home";
-export {default as NotFound} from "./NotFound";
-export {default as NavBar} from "./NavBar";
-export {default as Favorites} from "./Favorites";
-export {default as Users} from "./Users";
+export { default as Home } from "./Home";
+export { default as NotFound } from "./NotFound";
+export { default as NavBar } from "./NavBar";
+export { default as Favorites } from "./Favorites";
+export { default as Users } from "./Users";
