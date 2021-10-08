f8ea845a72ca5539d4396b2704f08ca650a36f60
Vlad Astakhov <68774820+AstakhovV@users.noreply.github.com>
2021-10-08 17:51:08 +0300

    Delete index.js

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as NotFound } from "./NotFound";
-export { default as NavBar } from "./NavBar";
-export { default as Favorites } from "./Favorites";
-export { default as Users } from "./Users";
